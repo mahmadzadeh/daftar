eb561c0f92ba2650aeff6decca8080529ae2c59e
mahmadzadeh <mahmadzadeh@gmail.com>
2018-03-17 11:13:17 -0700

    intial commit

---
+++ src/main/java/com/daftar/daftar/DaftarApplication.java
@@ -0,0 +1,12 @@
+package com.daftar.daftar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaftarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaftarApplication.class, args);
+	}
+}
---
+++ src/test/java/com/daftar/daftar/DaftarApplicationTests.java
@@ -0,0 +1,16 @@
+package com.daftar.daftar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DaftarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
